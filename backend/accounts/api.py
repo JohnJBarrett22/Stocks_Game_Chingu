f99f77a51d19ff58bda51273ca54222e1e7ab0e0
John Barrett <johnjbarrett22@gmail.com>
2019-06-20 05:18:35 -0700

    Completed UserAPI and functionality to validate tokens

--- backend/accounts/api.py
+++ backend/accounts/api.py
@@ -30,3 +30,11 @@ class LoginAPI(generics.GenericAPIView):
         })
 
 # GET USER API
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
