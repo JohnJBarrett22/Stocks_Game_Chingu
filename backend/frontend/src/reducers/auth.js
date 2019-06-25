17be780b04759c4b5522c37f61a76e6ac3af798a
John Barrett <johnjbarrett22@gmail.com>
2019-06-24 21:02:50 -0700

    Finalized login/registration, error alerts, logout functionality, changed nav bar for various states, etc.

--- backend/frontend/src/reducers/auth.js
+++ backend/frontend/src/reducers/auth.js
@@ -1,4 +1,4 @@
-import {USER_LOADING, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL } from '../actions/types';
+import {USER_LOADING, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT_SUCCESS, REGISTER_SUCCESS, REGISTER_FAIL} from '../actions/types';
 
 
 const initialState = {
@@ -9,7 +9,7 @@ const initialState = {
 }
 
 export default function(state = initialState, action) {
-    switch(action.type) {
+    switch (action.type) {
         case USER_LOADING:
             return {
                 ...state,
@@ -21,26 +21,29 @@ export default function(state = initialState, action) {
                 isAuthenticated: true,
                 isLoading: false,
                 user: action.payload
-            }
+            };
         case LOGIN_SUCCESS:
-            localStorage.setItem('token', action.payload.token);
+        case REGISTER_SUCCESS:
+            localStorage.setItem("token", action.payload.token);
             return {
                 ...state,
                 ...action.payload,
                 isAuthenticated: true,
                 isLoading: false
-            }
+            };
         case AUTH_ERROR:
         case LOGIN_FAIL:
-            localStorage.removeItem('token');
+        case LOGOUT_SUCCESS:
+        case REGISTER_FAIL:
+            localStorage.removeItem("token");
             return {
                 ...state,
                 token: null,
                 user: null,
                 isAuthenticated: false,
                 isLoading: false
-            }
-        default: 
+        };
+        default:
             return state;
     }
 }
